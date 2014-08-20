7e38412ba209f46a99b385276bf2ea07a456e5c4
softish <lazar.h.klincov@live.se>
2014-08-20 22:05:21 +0200

    added tank, extended carrying

---
+++ squad-deploy/Tank.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tank here.
+ * 
+ * @author softhish
+ * @version aug 2014
+ */
+public class Tank extends Mover
+{
+    public Tank()
+    {
+        scaleImage(getImage(), 2);
+    }
+    
+    /**
+     * Act - do whatever the Tank wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
